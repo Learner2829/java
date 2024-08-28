cb253c81e26ae57fe921f35ec89659e9f1520b80
ASHISH G PATNI <136146658+Learner2829@users.noreply.github.com>
2024-08-28 12:57:58 +0530

    Add files via upload
    28/8

---
+++ package/Other.java
@@ -0,0 +1,8 @@
+import my_package.Melo;
+class other{
+	public static void main(String args[]){
+		System.out.println("This is other file");
+		Melo m1= new Melo();
+		m1.method();
+	}
+}
---
+++ package/my_package/Melo.java
@@ -0,0 +1,12 @@
+// File: my_package/Melo.java
+package my_package;
+
+public class Melo {
+    public static void method() {
+        System.out.println("This is method call");
+    }
+    public static void main(String[] args) {
+        // Static method call does not need an object
+        method();
+    }
+}
